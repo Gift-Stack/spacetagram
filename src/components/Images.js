3d4ff4a41add1db2dd509c71f99ecd281505bbf3
Gift-Stack <gkenny896@gmail.com>
2021-09-20 20:17:57 +0100

    UI: Small screen implemented

--- src/components/Images.js
+++ src/components/Images.js
@@ -2,18 +2,17 @@ import UseAnimation from 'react-useanimations'
 import heart from 'react-useanimations/lib/heart'
 
 const Images = ({ images }) => {
-  console.log(images)
   return (
     <div className="body">
       {images.map((image) => (
         <div key={image.title} className="flex image-item">
           <img src={image.url} alt="Img Src" />
           <div>
-            <h3>
+            <h3 className="title">
               {image.title} - {image.date}
             </h3>
-            <p style={{ marginBottom: 60 }}>{image.explanation}</p>
-            <div style={{ cursor: 'pointer', display: 'inline-flex' }}>
+            <p className="explanation">{image.explanation}</p>
+            <div className="heart">
               <UseAnimation animation={heart} size={40} />
             </div>
           </div>
